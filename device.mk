332ade6912e2c6760507ef424bb59f5ef8720d9e
Mashopy <eliasgheeraert@gmail.com>
2026-07-06 18:34:04 +0200

    rosemary: Switch to common mediatek logtag properties
    Change-Id: Ie9ab821ec0d8e370215950029c0ed75699651ea4

--- device.mk
+++ device.mk
@@ -250,7 +250,7 @@ PRODUCT_PACKAGES += \
     PowerOffAlarm
 
 # Properties
-include $(LOCAL_PATH)/vendor_logtag.mk
+include hardware/mediatek/configs/properties/vendor_logtag.mk
 
 # Permissions
 PRODUCT_COPY_FILES += \
